f65e5a3cb2106b55863612bc2108c4163273036d
ardeleancristian <ardeleanc9@gmail.com>
2022-01-28 22:30:00 -0800

    update script

--- app/crypto_app.py
+++ app/crypto_app.py
@@ -6,24 +6,32 @@ from urllib.request import urlopen  # To add URLS
 st.write("""
 # Cryptocurrency Monitoring
 """)
-
+tickers =[]
 file=open('crypto_currencies.txt', 'r')
 ticker_list = file.read().splitlines()
-period= "6d"
+
 for ticker in ticker_list:
-  data=yf.Ticker(ticker)
-  data_ticker=data.history(period=period)
-  title= " """" ##  """ + ticker + " """ """"""
-  st.write(title)
-  imagePath = "static_images/"+ticker+".png"
-  image = Image.open(imagePath)
-  st.image(image, use_column_width=False)
-  st.line_chart(data_ticker.Close, use_container_width=True)
-  st.line_chart(data_ticker.Open, use_container_width=True)
-  st.line_chart(data_ticker.High, use_container_width=True)
-  st.line_chart(data_ticker.Low, use_container_width=True)
-  st.line_chart(data_ticker.Volume, use_container_width=True)
+  tickers.append(ticker)
+selectBox_key=0
+inputText_key=0
 
-
-
-
+option = st.selectbox(
+         'Select Crypto Currency',
+         (tickers), key = selectBox_key)
+period= st.text_input('Please tell me for what period you want to see market data (example: 1d, 1mo, 1y)', '', key=inputText_key)
+if (period == '[0-9]m')
+if (option):
+        data=yf.Ticker(option)
+        data_ticker=data.history(period=period)
+        title= " """" ##  """ + option + " """ """"""
+        st.write(title)
+        imagePath = "static_images/"+option+".png"
+        image = Image.open(imagePath)
+        st.image(image, use_column_width=False)
+        st.line_chart(data_ticker.Close, use_container_width=True)
+        st.line_chart(data_ticker.Open, use_container_width=True)
+        st.line_chart(data_ticker.High, use_container_width=True)
+        st.line_chart(data_ticker.Low, use_container_width=True)
+        st.line_chart(data_ticker.Volume, use_container_width=True)
+selectBox_key=0
+inputText_key=0
